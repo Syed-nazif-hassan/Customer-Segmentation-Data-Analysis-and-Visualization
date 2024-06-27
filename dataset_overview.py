56e48717ed57a3354d82a40ab5e7457ff7c20d3a
root <root@Nazif>
2024-06-27 22:24:52 +0600

    Modified 'data_conversion.py' and 'dataset_overview.py'.

--- dataset_overview.py
+++ dataset_overview.py
@@ -12,9 +12,9 @@ num_cols = len(df.columns)
 chunk_size = 4
 
 for i in range(0, num_cols, chunk_size):
-  group_cols = df.columns[i:i+chunk_size]
-  print(df[group_cols].describe(include='all'))
-  print("\n")
-  
+    group_cols = df.columns[i:i+chunk_size]
+    print(df[group_cols].describe(include='all'))
+    print("\n")
+
 print(df.isnull().sum())
 print(df.isna().any().any())
